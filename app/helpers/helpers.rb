9a9577afc2c2ac17bb29f9d9f24edc46e11b61a9
github username <github email address>
2019-01-21 12:17:56 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+  def self.current_user(session)
+    #binding.pry
+    User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def self.is_logged_in?(session)
+    !!self.current_user(session)
+  end
 end
